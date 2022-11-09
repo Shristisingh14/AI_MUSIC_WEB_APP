a4cef7ad2b56d1925378b4c166efb7bf819eecb6
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-11-09 15:31:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length >0)
+    {
+        console.log(results);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+    } 
 }
 
 song = "";
@@ -13,6 +42,7 @@ function preload() {
     song = loadSound("Dynamite.mp3");
     song2 = loadSound("Butter.mp3");
 }
+
 
 function draw() {
     image(video, 0, 0, 600, 500);
