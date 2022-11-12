ea65297767bda7f687629468281798b7d96619f6
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-11-12 15:48:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 leftWristX = 0;
 leftWristY = 0;
-
+scoreLeftWrist = 0;
 rightWristX = 0;
 rightWristY = 0;
+song_status = "";
 
 function setup() {
-    canvas = createCanvas(600, 500);
+    canvas = createCanvas(500, 400);
     canvas.center();
 
     video = createCapture(VIDEO);
@@ -25,6 +26,9 @@ function gotPoses(results) {
     {
         console.log(results);
 
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
@@ -39,11 +43,29 @@ song = "";
 song2 = "";
 
 function preload() {
-    song = loadSound("Dynamite.mp3");
+    song1 = loadSound("Dynamite.mp3");
     song2 = loadSound("Butter.mp3");
 }
 
 
 function draw() {
-    image(video, 0, 0, 600, 500);
+    image(video, 0, 0, 500, 400);
+
+    song_status = song1.isPlaying() ;
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        
+        song2.stop();
+
+        if(song_status == false)
+         {
+          song1.play();
+          document.getElementById("song_name").innerHTML = "Dynamite song is playing.";
+        }
+    }
 }
